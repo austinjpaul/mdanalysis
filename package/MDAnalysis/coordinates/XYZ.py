1fa5615168db643a0b80416c282e8310d7dc0f63
Oliver Beckstein <orbeckst@gmail.com>
2018-10-17 07:55:16 -0500

    Update package/MDAnalysis/coordinates/XYZ.py

--- package/MDAnalysis/coordinates/XYZ.py
+++ package/MDAnalysis/coordinates/XYZ.py
@@ -203,7 +203,7 @@ class XYZWriter(base.WriterBase):
         except AttributeError:
             if isinstance(obj, base.Timestep):
                 warnings.warn(
-                    'Passing a Timestep to write is deprecated, '
+                    'Passing a Timestep to write is deprecated, and will be removed in 1.0; '
                     'use either an AtomGroup or Universe',
                     DeprecationWarning)
                 ts = obj
